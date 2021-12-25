3649232ab99d82232dcbc186008440cd783bc2ce
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-12-25 15:33:37 +0600

    Style Added

--- src/Pages/UserEdit/UserEdit.js
+++ src/Pages/UserEdit/UserEdit.js
@@ -6,11 +6,11 @@ const UserEdit = () => {
     return (
         <div className='user-container'>
             <div className="usertitle">
-                <h1>Edit User</h1>
+                <h1>Create User</h1>
                 <button className='createNewUser'>Create</button>
             </div>
-            <div className="userEdit">
-                <h1>Edit</h1>
+            <div className="user-info-edit">
+                <h1>Edit User</h1>
                 <TextField id="standard-basic" defaultValue={"saanik96"} label="User Name" variant="standard" />
                 <TextField id="standard-basic" defaultValue={"Samiul Ahmed"} label="Full Name" variant="standard" />
                 <TextField id="standard-basic" defaultValue={"saanik96@gmail.com"} label="Email" variant="standard" />
@@ -18,7 +18,7 @@ const UserEdit = () => {
                 <TextField id="standard-basic" defaultValue={"New York | USA"} label="Address" variant="standard" />
                 <label htmlFor="myImage">Select Your Image</label>
                 <input type="file" id="myImage"/>
-                <input type="submit" value="Update" />
+                <input id='user-info-update' type="submit" value="Update" />
             </div>
         </div>
     );
